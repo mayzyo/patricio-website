d2a53461e447295c876a3328a484c221b005bb9e
Michael <michaelziyumay@hotmail.com>
2019-11-01 21:50:42 +0800

    feat: Removed i18n

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,11 @@
-import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-declare const require;
 
 if (environment.production) {
   enableProdMode();
 }
 
-function i18nProvider() {
-  if (localStorage.getItem('locale') === null) {
-    localStorage.setItem('locale', 'en');
-  }
-  
-  const locale = localStorage.getItem('locale');  
-  if(locale != 'en') {
-    document.documentElement.setAttribute('lang', locale);
-    const translations = require(
-      `raw-loader!./locale/messages.${locale}.xlf`
-    );
-  
-    return [
-      {provide: TRANSLATIONS, useValue: translations},
-      {provide: TRANSLATIONS_FORMAT, useValue: 'xlf'}
-    ];
-  }
-
-  return [];
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  providers: i18nProvider() // internationalisation
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
