aa7b3ec5bbaed2a1a140fd7d48bd1eeb2422496a
Michael <michaelziyumay@hotmail.com>
2020-02-21 17:03:13 +0800

    feat: blended banners

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Quote } from 'src/app/models/Quote';
-import { Moment } from 'src/app/models/Moment';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-banner',
@@ -15,14 +12,16 @@ export class BannerComponent implements OnInit {
   @Input() quote$: Observable<Quote>;
   @Input() backgroundUrl: string;
 
-  //readonly previews$ = this.http.get<Moment[]>('/api/media/gallery').pipe(
-  //  map(res => res.map(el => el.image))
-  //);
-
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
+  }
+
+  onStyle() {
+    return { 
+      background: `url(../../assets/images/${this.backgroundUrl})`, 
+      'background-size': 'cover', 
+      height: '55vh' 
+    };
   }
 }
