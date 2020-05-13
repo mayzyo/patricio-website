832124603c5b1cca6dd39152e7e3e3803cc21868
Michael <michaelziyumay@hotmail.com>
2020-05-13 17:23:55 +0800

    chore: added EF Core migrations

---
+++ Migrations/20200513090805_Initial.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PatricioPersonal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Area = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialMedia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Biography = table.Column<string>(nullable: true),
+                    Facebook = table.Column<string>(nullable: true),
+                    LinkedIn = table.Column<string>(nullable: true),
+                    Instagram = table.Column<string>(nullable: true),
+                    WeChat = table.Column<string>(nullable: true),
+                    Weibo = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialMedia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SocialMedia_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_Area",
+                table: "Owners",
+                column: "Area",
+                unique: true,
+                filter: "[Area] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialMedia_OwnerId",
+                table: "SocialMedia",
+                column: "OwnerId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SocialMedia");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
---
+++ Migrations/PatricioPersonalDb/20200513090834_Initial.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PatricioPersonal.Migrations.PatricioPersonalDb
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PatricioPersonalArticles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatricioPersonalArticles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatricioPersonalMoments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Thumbnail = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    ImageKey = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatricioPersonalMoments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatricioPersonalUpdates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    Thumbnail = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatricioPersonalUpdates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatricioPersonalMusics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: false),
+                    Genre = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Thumbnail = table.Column<string>(nullable: true),
+                    SoundCloud = table.Column<string>(nullable: true),
+                    CoverKey = table.Column<string>(nullable: true),
+                    AudioKey = table.Column<string>(nullable: true),
+                    Favourite = table.Column<bool>(nullable: false),
+                    ArticleId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatricioPersonalMusics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatricioPersonalMusics_PatricioPersonalArticles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "PatricioPersonalArticles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatricioPersonalMusics_ArticleId",
+                table: "PatricioPersonalMusics",
+                column: "ArticleId",
+                unique: true,
+                filter: "[ArticleId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PatricioPersonalMoments");
+
+            migrationBuilder.DropTable(
+                name: "PatricioPersonalMusics");
+
+            migrationBuilder.DropTable(
+                name: "PatricioPersonalUpdates");
+
+            migrationBuilder.DropTable(
+                name: "PatricioPersonalArticles");
+        }
+    }
+}
