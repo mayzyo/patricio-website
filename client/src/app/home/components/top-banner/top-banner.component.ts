c1872e67c0ef9d9387d499d291338f93ed68e282
Michael <25147960+darknesszy@users.noreply.github.com>
2021-01-04 21:28:43 +0800

    :clown_face: Proxy config for accessing API server

--- client/src/app/home/components/top-banner/top-banner.component.ts
+++ client/src/app/home/components/top-banner/top-banner.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-banner.component.scss']
 })
 export class TopBannerComponent implements OnInit {
+  test: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:5000/Songs?page=1&size=10').subscribe(res => {
+      this.test = res;
+      console.log('tester', res[0])
+      
+    })
   }
 
 }
