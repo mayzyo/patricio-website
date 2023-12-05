6d0421f56ee1ce60b446deb28031bb7e9ecc665f
mayzyo <25147960+mayzyo@users.noreply.github.com>
2023-12-05 20:39:51 +1000

    :sparkles: Added Loading indicator for lazy load

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
 import { MainModule } from './main/main.module';
 import { HomeModule } from './home/home.module';
 import { EditorService } from './admin/services/editor.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,15 @@ import { EditorService } from './admin/services/editor.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     @ViewChild("EditorRef", { read: ViewContainerRef }) editorRef?: ViewContainerRef;
 
-    constructor(private editor: EditorService) {}
+    routeLoading$ = this.router.events.pipe(
+        filter(event => event instanceof RouteConfigLoadStart || event instanceof RouteConfigLoadEnd),
+        map(event => event instanceof RouteConfigLoadStart)
+    );
+
+    constructor(private router: Router, private editor: EditorService) {}
 
     ngAfterViewInit(): void {
         if(this.editorRef) {
