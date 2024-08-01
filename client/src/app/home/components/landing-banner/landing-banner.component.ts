a3decc7cf17d92165ee6205280f7a03d0bd763e9
micha <xn--158h@xn--dx8h.to>
2024-08-01 22:33:27 +0000

    :bug: Removed arrow hightlight

--- client/src/app/home/components/landing-banner/landing-banner.component.ts
+++ client/src/app/home/components/landing-banner/landing-banner.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, input, output, ViewChild } from '@angular/core';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { QuoteService } from '../../../shared/services/quote.service';
 import { delay } from 'rxjs/operators';
@@ -12,11 +12,8 @@ import { delay } from 'rxjs/operators';
 export class LandingBannerComponent {
     @ViewChild('ScrollAnchor', { static: true }) scrollAnchor?: ElementRef<HTMLElement>;
 
-    protected readonly _arrowDisabled = signal(false);
-    @Input() set arrowDisabled(value: boolean) {
-        this._arrowDisabled.set(value);
-    }
-    @Output() scrolldownChange = new EventEmitter<void>();
+    arrowDisabled = input<boolean>(false);
+    scrolldownChange = output<void>();
 
     protected readonly faArrowDown = faArrowDown;
     protected readonly quote$ = this.quote.unique$('landing').pipe(delay(1000));
